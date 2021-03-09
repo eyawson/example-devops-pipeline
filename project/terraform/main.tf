ff5cfa1f71dfc18322bb929633141fb5cae3c1c3
eyawson <ebow.yawson@gmail.com>
2021-03-09 10:52:11 -0500

    moved prereq bash file into terraform folder

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "main_server" {
   ami           = var.aws_ami
   instance_type = var.instance_type
-  user_data     = "${file(server_prereqs.sh)}"
+  user_data     = file("server_prereqs.sh")
 
   tags = {
     Name = "Main Server"
